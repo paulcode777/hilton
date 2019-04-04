a8b7b92b808f855b20029babfe1d4e268acbf9c9
paulcode777 <paulcode777>
2019-04-04 15:58:38 -0400

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## Test 1:
+Simply drag and drop the index.html file onto any web browser.
+
+## Test 2: 
+Install Serve with: 
+```bash
+npm install -g serve
+```
+
+From the hilton-assessment folder: 
+```bash
+serve -s test2
+```
---
+++ test2/precache-manifest.656f7e3a895476a150b18c60b0de7886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2395026cd95c71f7a0a4",
+    "url": "/static/js/main.3c8c8921.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2395026cd95c71f7a0a4",
+    "url": "/static/css/main.e8abe164.chunk.css"
+  },
+  {
+    "revision": "73cbf48b5cac68d48d896f03eff8f940",
+    "url": "/index.html"
+  }
+];
